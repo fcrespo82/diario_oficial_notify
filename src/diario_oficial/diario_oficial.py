7372b9c9405c23e7922b50d00445760660915f41
Fernando Crespo <fernando82@gmail.com>
2025-04-28 11:43:54 -0300

    Atualiza workflow

--- src/diario_oficial/diario_oficial.py
+++ src/diario_oficial/diario_oficial.py
@@ -7,7 +7,6 @@ import logging
 import json
 from typing import Any
 import urllib.parse
-from bs4 import BeautifulSoup
 import requests
 
 from .result import Result
@@ -32,34 +31,27 @@ class DiarioOficial:
         '''
         txt_keyword_quoted = f'"{txt_keyword}"'
         palavra_chave = urllib.parse.quote_plus(txt_keyword_quoted)
-        periodo = urllib.parse.quote_plus(
-            f'{date_from.strftime("%d/%m/%Y")} a {date_to.strftime("%d/%m/%Y")}')
 
-        print(
-            f'Buscando por {txt_keyword_quoted} de {date_from.strftime("%d/%m/%Y")} até {date_to.strftime("%d/%m/%Y")}')
-        url = f'http://www.imprensaoficial.com.br/DO/BuscaDO2001Resultado_11_3.aspx?\
-filtropalavraschave={palavra_chave}&\
-filtroperiodo={periodo}&\
-filtrogrupos=Legislativo+&\
-filtrotodosgrupos=False&\
-CampoOrdenacao=datapublicacao&\
-DirecaoOrdenacao=descending'
+        print(f'Buscando por {txt_keyword_quoted} de {date_from.strftime("%d/%m/%Y")} até {date_to.strftime("%d/%m/%Y")}')
+        url = f'https://do-api-web-search.doe.sp.gov.br/v2/advanced-search/publications?\
+periodStartingDate={date_from.strftime("%Y-%m-%d")}&\
+FromDate={date_from.strftime("%Y-%m-%d")}&\
+ToDate={date_to.strftime("%Y-%m-%d")}&\
+JournalId=0953858b-7195-4020-ec15-08db6b8e0e4c&\
+Terms%5B0%5D={palavra_chave}'
 
         self.log.debug(url)
         response = requests.get(url, timeout=60)
-        soup = BeautifulSoup(response.content, features='html5lib')
-        mensagem = soup.find(id='content_lblMensagem')
-        self.log.debug(soup.prettify())
-        if mensagem:
-            print('Nenhum resultado encontrado')
-            return []
 
-        results = soup.find_all('div', {'class': 'resultadoBuscaItem'})
+        response_json = json.loads(response.text)
+
+        results = response_json["items"]
+
         self.log.debug(results)
         print(
             f'Encontrado {len(results)} resultado{"s" if len(results) > 1 else ""}')
 
-        objs = list(map(self._extract_info, results))
+        objs = list(map(self._extract_info_from_json, results))
 
         objs = list(map(self._set_title, objs, [txt_keyword]*len(objs)))
 
@@ -91,6 +83,22 @@ DirecaoOrdenacao=descending'
         self.log.info(json.dumps((text, url, title.text), indent=4))
         return Result(text, url, title.text)
 
+    def _extract_info_from_json(self, result: dict):
+        '''
+        Extrai informações do resultado da busca
+        '''
+        title = result['title']
+        self.log.info(title)
+        link = result['slug']
+        self.log.debug(link)
+        url = f'https://do-api-web-search.doe.sp.gov.br/{link}'
+        self.log.info(url)
+        text = result['excerpt']
+        self.log.info(text)
+
+        self.log.info(json.dumps((text, url, title), indent=4))
+        return Result(text, url, title)
+    
     def busca_dia(self, txt_keyword: str, date:datetime.datetime=datetime.datetime.now()):
         '''
         Busca o texto no diário oficial para um dia específico
